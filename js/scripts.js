77a98520df4b3e3bdae51f8818d706c782e77523
Emanuela Stetsko <emanuelastetsko.es@gmail.com>
2022-12-21 13:36:44 +0100

    Alert di benvenuto + raccolta dati

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// Benvenuto
+alert("Benvenuto sul sito di Trenitalia!");
+
+// Info 
+alert("Nei prossimi passaggi ti saranno richiesti i dati necessari per il calcolo del prezzo del tuo biglietto")
+
+// Chiedo il numero di km che vuole fare 
+const numerokm = prompt("Quanti km percorrerai?");
+console.log("Il passeggerò percorrerà: ", numerokm);
+
+// Chiedo l'età al passeggero 
+const etàpasseggero = prompt("Quanti anni hai?");
+console.log("Il passeggerò ha: ", etàpasseggero);
